fd88dee2136ad904cdf0847e3ae02cd769152793
Gijsjan <agijsbro@gmail.com>
2016-04-11 14:32:15 +0200

    Bump hire-forms-checkbox

--- build/index.js
+++ build/index.js
@@ -1,55 +1,5 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.HireFormsMultiSelect = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-/*!
-  Copyright (c) 2015 Jed Watson.
-  Licensed under the MIT License (MIT), see
-  http://jedwatson.github.io/classnames
-*/
-
-(function () {
-	'use strict';
-
-	function classNames () {
-
-		var classes = '';
-
-		for (var i = 0; i < arguments.length; i++) {
-			var arg = arguments[i];
-			if (!arg) continue;
-
-			var argType = typeof arg;
-
-			if ('string' === argType || 'number' === argType) {
-				classes += ' ' + arg;
-
-			} else if (Array.isArray(arg)) {
-				classes += ' ' + classNames.apply(null, arg);
-
-			} else if ('object' === argType) {
-				for (var key in arg) {
-					if (arg.hasOwnProperty(key) && arg[key]) {
-						classes += ' ' + key;
-					}
-				}
-			}
-		}
-
-		return classes.substr(1);
-	}
-
-	if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
-		// AMD. Register as an anonymous module.
-		define(function () {
-			return classNames;
-		});
-	} else if (typeof module !== 'undefined' && module.exports) {
-		module.exports = classNames;
-	} else {
-		window.classNames = classNames;
-	}
-
-}());
-
-},{}],2:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.HireFormsCheckbox = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -58,11 +8,13 @@ Object.defineProperty(exports, "__esModule", {
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; }
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var _react = require("react");
 
@@ -76,15 +28,13 @@ var _classnames2 = _interopRequireDefault(_classnames);
 var CHECKBOX = "hire-checkbox";
 
 var Checkbox = (function (_React$Component) {
+	_inherits(Checkbox, _React$Component);
+
 	function Checkbox() {
 		_classCallCheck(this, Checkbox);
 
-		if (_React$Component != null) {
-			_React$Component.apply(this, arguments);
-		}
+		_get(Object.getPrototypeOf(Checkbox.prototype), "constructor", this).apply(this, arguments);
 	}
-
-	_inherits(Checkbox, _React$Component);
 
 	_createClass(Checkbox, [{
 		key: "handleClick",
@@ -111,27 +61,43 @@ var Checkbox = (function (_React$Component) {
 					onClick: this.handleClick.bind(this) },
 				_react2["default"].createElement(
 					"svg",
-					{ viewBox: "0 0 220 220" },
-					_react2["default"].createElement("rect", {
-						fillOpacity: "0",
-						height: "190",
-						rx: "20",
-						ry: "20",
-						stroke: "black",
-						strokeWidth: "20",
-						width: "190",
-						x: "15",
-						y: "15" }),
-					_react2["default"].createElement("rect", {
-						height: "200",
-						width: "20",
-						x: "180",
-						y: "0" }),
-					_react2["default"].createElement("rect", {
-						height: "20",
-						width: "200",
-						x: "0",
-						y: "180" })
+					{ viewBox: "0 0 240 220" },
+					_react2["default"].createElement(
+						"g",
+						{ className: "box" },
+						_react2["default"].createElement("rect", {
+							fillOpacity: "0",
+							height: "190",
+							rx: "20",
+							ry: "20",
+							stroke: "black",
+							strokeWidth: "20",
+							width: "190",
+							x: "15",
+							y: "15" })
+					),
+					_react2["default"].createElement(
+						"g",
+						{ className: "checkmark" },
+						_react2["default"].createElement("rect", {
+							fill: "#00BFA5",
+							height: "212",
+							rx: "10",
+							ry: "10",
+							transform: "rotate(45)",
+							width: "60",
+							x: "150",
+							y: "-130" }),
+						_react2["default"].createElement("rect", {
+							fill: "#00BFA5",
+							height: "120",
+							rx: "10",
+							ry: "10",
+							transform: "rotate(-45)",
+							width: "60",
+							x: "-90",
+							y: "90" })
+					)
 				),
 				label
 			);
@@ -154,7 +120,9 @@ Checkbox.propTypes = {
 exports["default"] = Checkbox;
 module.exports = exports["default"];
 
-},{"classnames":1,"react":"react"}],3:[function(require,module,exports){
+},{"classnames":"classnames","react":"react"}]},{},[1])(1)
+});
+},{"classnames":"classnames","react":"react"}],2:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -163,11 +131,13 @@ Object.defineProperty(exports, "__esModule", {
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; }
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var _react = require("react");
 
@@ -178,15 +148,13 @@ var _hireFormsCheckbox = require("hire-forms-checkbox");
 var _hireFormsCheckbox2 = _interopRequireDefault(_hireFormsCheckbox);
 
 var MultiSelect = (function (_React$Component) {
+	_inherits(MultiSelect, _React$Component);
+
 	function MultiSelect() {
 		_classCallCheck(this, MultiSelect);
 
-		if (_React$Component != null) {
-			_React$Component.apply(this, arguments);
-		}
+		_get(Object.getPrototypeOf(MultiSelect.prototype), "constructor", this).apply(this, arguments);
 	}
-
-	_inherits(MultiSelect, _React$Component);
 
 	_createClass(MultiSelect, [{
 		key: "handleChange",
@@ -241,5 +209,5 @@ MultiSelect.propTypes = {
 exports["default"] = MultiSelect;
 module.exports = exports["default"];
 
-},{"hire-forms-checkbox":2,"react":"react"}]},{},[3])(3)
+},{"hire-forms-checkbox":1,"react":"react"}]},{},[2])(2)
 });
--- scripts/watch.sh
+++ scripts/watch.sh
@@ -4,6 +4,7 @@ node_modules/.bin/watchify src/index.jsx \
 	--detect-globals false \
 	--extension=.jsx \
 	--external react \
+	--external classnames \
 	--outfile build/index.js \
 	--standalone HireFormsMultiSelect \
 	--transform [ babelify --plugins object-assign ] \
